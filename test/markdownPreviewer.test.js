3413af9e6e2c745a75f8edb24287451958606e0f
ilake <lake.ilakela+github@gmail.com>
2019-04-03 22:11:49 +0800

    Use babel to write modern javascript

--- test/markdownPreviewer.test.js
+++ test/markdownPreviewer.test.js
@@ -1,7 +1,17 @@
 import markdownPreviewer from "../js/markdownPreviewer"
 
-describe("markdownPreviewer", function() {
-  it("should exist", function() {
+describe("markdownPreviewer", () => {
+  it("should exist", () => {
     expect(markdownPreviewer).toBeDefined();
+  });
+
+  it("should allow deep references", () => {
+    const o = {
+      foo: {
+        bar: "blah"
+       }
+    };
+
+    expect((o?.foo?.bar)).toBe("blah");
   });
 });
